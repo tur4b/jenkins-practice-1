fa11eb8573b20b918bc9898bc5f76fad46c23454
Turab Eybaliyev <eybaliyev98@gmail.com>
2022-07-26 12:53:13 +0400

    classes updated new tests were written

---
+++ src/main/java/com/practice/service/MathOperations.java
@@ -0,0 +1,69 @@
+package com.practice.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Slf4j
+@Service
+public class MathOperations {
+
+    public BigDecimal add(BigDecimal... nums) {
+        verifyRequiredNumsByLength(nums);
+
+        BigDecimal base = BigDecimal.ZERO;
+        for (BigDecimal num : nums) {
+            base = base.add(num);
+        }
+        return base;
+    }
+
+
+    public BigDecimal subtract(BigDecimal base, BigDecimal... nums) {
+        verifyRequiredNumsByLength(nums);
+
+        for (BigDecimal num : nums) {
+            base = base.subtract(num);
+        }
+        return base;
+    }
+
+    public BigDecimal multiply(BigDecimal base, BigDecimal... nums) {
+        verifyRequiredNumsByLength(nums);
+
+        if(base.compareTo(BigDecimal.ZERO) == 0) {
+            return base;
+        }
+
+        for (BigDecimal num : nums) {
+            base = base.multiply(num);
+        }
+        return base;
+    }
+
+    public BigDecimal divide(BigDecimal base, BigDecimal... nums) {
+        verifyRequiredNumsByLength(nums);
+
+        if(base.compareTo(BigDecimal.ZERO) == 0) {
+            return base;
+        }
+
+        for (BigDecimal num : nums) {
+            if(num.compareTo(BigDecimal.ZERO) == 0) {
+                throw new ArithmeticException("Can't divide by zero!");
+            }
+            base = base.divide(num, 2, RoundingMode.CEILING);
+        }
+        return base;
+    }
+
+
+    private void verifyRequiredNumsByLength(BigDecimal[] nums) {
+        if(nums.length == 0) {
+            throw new IllegalArgumentException("At least one number must be exist!");
+        }
+    }
+
+}
---
+++ src/test/java/com/practice/service/MathOperationsTest.java
@@ -0,0 +1,111 @@
+package com.practice.service;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathOperationsTest {
+
+    private final MathOperations mathOperations = new MathOperations();
+
+    @Test
+    void add() {
+
+        final BigDecimal one = BigDecimal.ONE;
+        final BigDecimal five = BigDecimal.valueOf(5);
+        final BigDecimal ten = BigDecimal.TEN;
+
+        final BigDecimal sumResult = mathOperations.add(one, five, ten);
+        final BigDecimal expected = BigDecimal.valueOf(16);
+
+        assertTrue(expected.compareTo(sumResult) == 0);
+    }
+
+    @Test
+    void add_with_invalid_arg_count_throws_exception() {
+
+        final IllegalArgumentException exc = assertThrows(IllegalArgumentException.class,
+                () -> mathOperations.add()
+        );
+
+        assertEquals("At least one number must be exist!", exc.getMessage());
+    }
+
+    @Test
+    void subtract() {
+
+        final BigDecimal one = BigDecimal.ONE;
+        final BigDecimal five = BigDecimal.valueOf(5);
+        final BigDecimal ten = BigDecimal.TEN;
+
+        final BigDecimal subtractResult = mathOperations.subtract(ten, five, one);
+        final BigDecimal expected = BigDecimal.valueOf(4);
+
+        assertTrue(expected.compareTo(subtractResult) == 0);
+    }
+
+    @Test
+    void subtract_with_invalid_arg_count_throws_exception() {
+
+        final IllegalArgumentException exc = assertThrows(IllegalArgumentException.class,
+                () -> mathOperations.subtract(BigDecimal.TEN)
+        );
+
+        assertEquals("At least one number must be exist!", exc.getMessage());
+    }
+
+    @Test
+    void multiply() {
+        final BigDecimal one = BigDecimal.ONE;
+        final BigDecimal five = BigDecimal.valueOf(5);
+        final BigDecimal ten = BigDecimal.TEN;
+
+        final BigDecimal multiplyResult = mathOperations.multiply(ten, five, one);
+        final BigDecimal expected = BigDecimal.valueOf(50);
+
+        assertTrue(expected.compareTo(multiplyResult) == 0);
+    }
+
+    @Test
+    void multiply_with_invalid_arg_count_throws_exception() {
+        final IllegalArgumentException exc = assertThrows(IllegalArgumentException.class,
+                () -> mathOperations.multiply(BigDecimal.TEN)
+        );
+
+        assertEquals("At least one number must be exist!", exc.getMessage());
+    }
+
+
+    @Test
+    void divide() {
+        final BigDecimal one = BigDecimal.ONE;
+        final BigDecimal three = BigDecimal.valueOf(3);
+        final BigDecimal fourteen = BigDecimal.valueOf(14);
+
+        final BigDecimal divideResult = mathOperations.divide(fourteen, three, one);
+        final BigDecimal expected = BigDecimal.valueOf(4.67);
+
+        assertEquals(expected, divideResult);
+    }
+
+    @Test
+    void divide_with_invalid_arg_count_throws_exception() {
+        final IllegalArgumentException exc = assertThrows(IllegalArgumentException.class,
+                () -> mathOperations.divide(BigDecimal.TEN)
+        );
+
+        assertEquals("At least one number must be exist!", exc.getMessage());
+    }
+
+    @Test
+    void divide_by_zero_throws_exception() {
+        final ArithmeticException exc = assertThrows(ArithmeticException.class,
+                () -> mathOperations.divide(BigDecimal.TEN, BigDecimal.ZERO, BigDecimal.ONE)
+        );
+
+        assertEquals("Can't divide by zero!", exc.getMessage());
+    }
+
+}
